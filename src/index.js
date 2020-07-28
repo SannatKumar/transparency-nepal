66990874d33a25b4c158e729dafa676f0569669f
Raj Kumar <interplaytheheads@gmail.com>
2020-07-28 21:46:32 +0200

    Header Footer imported

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
