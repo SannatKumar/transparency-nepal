bb75a6bf325323539f78547ab1fea24a084a0fcb
Raj Kumar <interplaytheheads@gmail.com>
2020-07-29 12:30:16 +0200

    Header changed with bootstrap components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Header.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Navbar} from 'react-bootstrap';
+
 
 class Header extends React.Component{
     render(){
-  return (
-    <div className = "Header">
-      <h1>Corona Cases in Nepal</h1>
-    </div>
+      return (
+      <div className = "Header">      
+        <Navbar bg="dark" variant="dark">
+                  <Navbar.Brand href="#home">
+                  Corona Virus Cases In Nepal
+                  </Navbar.Brand>
+              </Navbar>
+      </div>
   );
 }
 }
