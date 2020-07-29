84902aafa636b7538e01444ac2199fe348c225ad
Raj Kumar <interplaytheheads@gmail.com>
2020-07-29 11:46:19 +0200

    Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,10 @@ class Footer extends React.Component{
   return (
     <div className = "Footer">
       <h1>Contacts</h1>
+      <h1>About us</h1>
+      <h1>Social Media</h1>
+      <h1>Developer</h1>
+      <h1>@Software developed in Finland</h1>
     </div>
   );
 }
