6071a98994243a541c7f02baa74ecceaeb58779e
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 01:04:28 +0200

    New Nav Bar added for the proper alignment of content

--- src/App.js
+++ src/App.js
@@ -11,22 +11,30 @@ class App extends React.Component {
         <div class="container">
             <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/coronaresult/">Nepal Corona Result</Link>
-            </li>
-            <li>
-              <Link to="/osresult/">Click to see your operating system</Link>
-            </li>
-            <li>
-              <Link to="/portfolio/">My Portfolio</Link>
-            </li>
-          </ul>
-        </nav>
+        
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/">JagratNepal.Com</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item nav-link active">
+      <Link to="/">Home</Link>
+      </li>
+      <li class="nav-item nav-link">
+      <Link to="/coronaresult/">Nepal Corona Result</Link>
+      </li>
+      <li class="nav-item nav-link">
+      <Link to="/osresult/">Your OS</Link>
+      </li>
+      <li class="nav-item nav-link">
+      <Link to="/portfolio/">My Portfolio</Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+          
         <Route path="./home" exact component={App} />
         <Route path="/coronaresult" exact component={CoronaResult} />
         <Route path="/osresult" exact component={OsResult} />
