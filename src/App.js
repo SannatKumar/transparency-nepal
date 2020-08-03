5890fa305b7d60368191a25728a654624f51d323
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 00:50:08 +0200

    Portfolio Link Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import CoronaResult from './CoronaResult';
 import OsResult from './OsResult';
+import PortFolio from './PortFolio';
 import './App.css';
 
 class App extends React.Component {
@@ -21,11 +22,15 @@ class App extends React.Component {
             <li>
               <Link to="/osresult/">Click to see your operating system</Link>
             </li>
+            <li>
+              <Link to="/portfolio/">My Portfolio</Link>
+            </li>
           </ul>
         </nav>
         <Route path="./home" exact component={App} />
         <Route path="/coronaresult" exact component={CoronaResult} />
         <Route path="/osresult" exact component={OsResult} />
+        <Route path="/portfolio" exact component={PortFolio} />
 
       </div>
     </Router>
