762cd796ddb5853c9a6667e2700374a655910b9a
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 18:46:44 +0200

    All the links shifted to Header and styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,60 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Navbar} from 'react-bootstrap';
-import rajkumar from '../rajkumar.jpg';
+import { Navbar } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CoronaResult from "../CoronaResult";
+import OsResult from "../OsResult";
+import PortFolio from "../PortFolio";
+import App from "../App";
 
-
-class Header extends React.Component{
-    render(){
-      return (
-      <div className = "Header">      
-        <Navbar bg="dark" variant="dark">
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header">
+          <Router>
+                <Navbar bg="dark" variant="dark">
                   <Navbar.Brand href="#home">
-                  <div className = "pic-title">
-                    <img src={rajkumar} className="profile-pic" alt="rajkumar" />
-                    <br /><br />
-                    <h2 className = "username">Raj Kumar Tiwari</h2>
-                  </div>
+                    <a class="navbar-brand" href="/app">
+                      JagratNepal.Com
+                    </a>
+                    <button
+                      class="navbar-toggler"
+                      type="button"
+                      data-toggle="collapse"
+                      data-target="#navbarNav"
+                      aria-controls="navbarNav"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                      <ul class="navbar-nav">
+                        <li class="nav-item nav-link active">
+                          <Link to="/app/">Home</Link>
+                        </li>
+                        <li class="nav-item nav-link">
+                          <Link to="/coronaresult/">Nepal Corona Result</Link>
+                        </li>
+                        <li class="nav-item nav-link">
+                          <Link to="/osresult/">Your OS</Link>
+                        </li>
+                        <li class="nav-item nav-link">
+                          <Link to="/portfolio/">My Portfolio</Link>
+                        </li>
+                      </ul>
+                    </div>
                   </Navbar.Brand>
-              </Navbar>
-      </div>
-  );
-}
+                </Navbar>
+              <Route path="/app" exact component={App} />
+              <Route path="/coronaresult" exact component={CoronaResult} />
+              <Route path="/osresult" exact component={OsResult} />
+              <Route path="/portfolio" exact component={PortFolio} />
+          </Router>
+        </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
