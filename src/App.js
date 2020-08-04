0543c93ab21121766e7294026b4329599b057925
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 18:44:26 +0200

    Content changed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import CoronaResult from './CoronaResult';
-import OsResult from './OsResult';
-import PortFolio from './PortFolio';
-import './App.css';
+import React from "react";
+
 
 class App extends React.Component {
   render() {
     return (
-        <div class="container">
-            <Router>
-      <div>
-        
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">JagratNepal.Com</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item nav-link active">
-      <Link to="/">Home</Link>
-      </li>
-      <li class="nav-item nav-link">
-      <Link to="/coronaresult/">Nepal Corona Result</Link>
-      </li>
-      <li class="nav-item nav-link">
-      <Link to="/osresult/">Your OS</Link>
-      </li>
-      <li class="nav-item nav-link">
-      <Link to="/portfolio/">My Portfolio</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-          
-        <Route path="./home" exact component={App} />
-        <Route path="/coronaresult" exact component={CoronaResult} />
-        <Route path="/osresult" exact component={OsResult} />
-        <Route path="/portfolio" exact component={PortFolio} />
-
-      </div>
-    </Router>
-        </div>
+      <div className="CoronaResult">
+            <table className = "table table-striped" id="coronaresultdisplay" aria-labelledby="tabelLabel">
+                <tr>
+                  <th>Headers</th>
+                  <th>Data</th>
+                </tr>
+                <tr>
+                  <td>Projects</td>
+                  <td>Links</td>               
+                </tr>
+                <tr>
+                  <td>PHP</td>
+                  <td>Links</td>              
+                </tr>
+                <tr>
+                  <td>Sunday</td>
+                  <td>Sununtai</td>              
+                </tr>
+                <tr>
+                  <td>Monday</td>
+                  <td>Manantai</td>              
+                </tr>
+                <tr>
+                  <td>Tuesday</td>
+                  <td>Tiistai</td>              
+                </tr>
+                <tr>
+                  <td>Wednesday</td>
+                  <td>Keskivikko</td>              
+                </tr>
+                <tr>
+                  <td>Thursday</td>
+                  <td>Torstai</td>              
+                </tr>
+                <tr>
+                  <td>Friday</td>
+                  <td>Perjantai</td>              
+                </tr>
+                <tr>
+                  <td>Saturday</td>
+                  <td>Launtai</td>              
+                </tr>            
+              </table>    
+          </div>
+      
     );
+  }
 }
-}
-
 
 export default App;
