36fdbe168cac1b80a7f655517d3da89dc9ca7b0b
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 21:22:39 +0200

    Updates

---
+++ precache-manifest.d0372f5da8b6bf7e5e08d9399ffa457a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f9a3e0e3f50ea803a884815c7b77c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "897d2cd259ca205ff4b0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ce1a0c85d17a62d82d2a",
+    "url": "/static/css/main.bbbae81c.chunk.css"
+  },
+  {
+    "revision": "897d2cd259ca205ff4b0",
+    "url": "/static/js/2.3e953ef2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3e953ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1a0c85d17a62d82d2a",
+    "url": "/static/js/main.68386148.chunk.js"
+  },
+  {
+    "revision": "4ffde7c6e3e636f9bc66",
+    "url": "/static/js/runtime-main.564bf12a.js"
+  },
+  {
+    "revision": "e86a9fe6fbfe6dd6b4e224e6d3dbf13e",
+    "url": "/static/media/rajkumar.e86a9fe6.jpg"
+  }
+]);
