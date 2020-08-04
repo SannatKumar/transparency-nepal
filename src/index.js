37f84871d0e0f21e7fff67ff133841082317dc73
Raj Kumar <interplaytheheads@gmail.com>
2020-08-04 18:47:32 +0200

    App removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import * as serviceWorker from './serviceWorker';
@@ -9,7 +8,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <App />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
