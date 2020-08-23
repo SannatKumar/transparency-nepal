bf2be48f13d743066842c2dfbaf1dc973d7f5f8e
Raj Kumar <interplaytheheads@gmail.com>
2020-08-23 21:15:53 +0200

    tried new code and removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
