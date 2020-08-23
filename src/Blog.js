7bab4a5e8fe554a54d8323c12bdec3b8b1ddf9c9
Raj Kumar <interplaytheheads@gmail.com>
2020-08-23 21:13:37 +0200

    New Blog Page Created

---
+++ src/Blog.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+
+
+class Blog extends Component{
+    render(){
+        return(
+            <div className = "mainblog">
+            <div className="mainwrap">
+                <div className="topbar">
+                    <div className = "mainhead">
+                        <div className = "bodywrap">
+                            <div className = "row">
+                                <div className = "col-8">
+                                    <p> Here will be the Blog</p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        )
+    }
+
+}
+
+export default Blog;
