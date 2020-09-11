cc9a43e6b61734488e8bb4a4a1964230675703c4
Raj Kumar <interplaytheheads@gmail.com>
2020-09-11 22:23:14 +0200

    App component for testing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 
 class App extends Component {
     render() {
         return ( 
-        < div className = "Main" > 
-          <div className="CoronaResult">
-            <div class="container">
-
-             <div id="navbar-collapse" class="navbar-collapse collapse">
-                            <div class="region region-navigation-collapsible">
-                                <nav role="navigation" aria-labelledby="block-blogstheme-main-menu-menu"
-                                    id="block-blogstheme-main-menu">
-                                    <h2 class="sr-only" id="block-blogstheme-main-menu-menu">Main navigation</h2>
-
-                                    <ul class="menu menu--main nav navbar-nav">
-                                        <li class="first">
-                                            <a href="/" data-drupal-link-system-path="&lt;front&gt;">Home</a>
-                                        </li>
-                                        <li>
-                                            <a href="/blogs" data-drupal-link-system-path="blogs">All Blogs</a>
-                                        </li>
-                                        <li>
-                                            <a href="/topics" data-drupal-link-system-path="topics">Topics</a>
-                                        </li>
-                                        <li>
-                                            <a href="/contact-us" data-drupal-link-system-path="contact/main_contact_us">Contact</a>
-                                        </li>
-                                        <li class="last">
-                                            <a href="/search" data-drupal-link-system-path="search">Search</a>
-                                        </li>
-                                    </ul>
-
-                                </nav>
-                                </div>
-                                </div>
-                                </div>
-                                </div>
-                                </div>
-                                
-                                      )
+            
+        < div className = "Main" >  
+            <h1> Dont Show</h1>
+        </div>                                
+        )
+    }
 }
-}
-                                      export default App;
+export default App;
